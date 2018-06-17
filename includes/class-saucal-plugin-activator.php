ba11bad3e44f5f2c9e8a2a5ddecd2062b8d69939
Jonny Rudd <jonny.rudd@genesissports.co.uk>
2018-06-17 13:02:02 +0100

    Initial Plugin Boilerplate Import

---
+++ includes/class-saucal-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.jonnyrudd.co.uk
+ * @since      1.0.0
+ *
+ * @package    Saucal_Plugin
+ * @subpackage Saucal_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Saucal_Plugin
+ * @subpackage Saucal_Plugin/includes
+ * @author     Jonny Rudd <jonny@jonnyrudd.co.uk>
+ */
+class Saucal_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-saucal-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.jonnyrudd.co.uk
+ * @since      1.0.0
+ *
+ * @package    Saucal_Plugin
+ * @subpackage Saucal_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Saucal_Plugin
+ * @subpackage Saucal_Plugin/includes
+ * @author     Jonny Rudd <jonny@jonnyrudd.co.uk>
+ */
+class Saucal_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
